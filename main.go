a4c111147cbadaeba1dfcc047b9eff6740b33a28
Naphatsaminh Sirichai <>
2021-08-11 15:32:26 +0700

    no message

--- main.go
+++ main.go
@@ -2,17 +2,15 @@ package main
 
 import (
 	"github.com/labstack/echo/v4"
-	"net/http"
 )
 
 func main() {
 
 
 	e := echo.New()
-	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "ok")
-	})
-
+	//e.GET("/", func(c echo.Context) error {
+	//	return c.String(http.StatusOK, "ok")
+	//})
 	e.POST("/webhook", WebHook)
 	e.Logger.Fatal(e.Start(":1323"))
 
